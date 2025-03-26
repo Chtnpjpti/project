44284c82bd7ee9e9d7a3a8b5716805ac7f86cbb2
Chtnpjpti <145962452+Chtnpjpti@users.noreply.github.com>
2025-03-26 12:40:10 +0530

    Add files via upload

---
+++ AddNumbers.java
@@ -0,0 +1,96 @@
+public class AddNumbers{
+    public static void main(String[] args) {
+        int num1 = 5, num2 = 10, sum;   
+        sum = num1 + num2;              
+        System.out.println(sum);   
+    }
+}
+
+
+
+//  EXAMPLE :- 4
+
+// public class MaxOfThreeNumbers{
+    //     public static void main(String[] args) {
+        //     int num1 = 10, num2 = 20, num3 = 15, max;
+        //     max = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+        //     System.out.println("Maximum of " + num1 + ", " + num2 + ", and "+ num3 + " is: " + max);
+        //     }
+        //    }
+        
+
+
+        //  EXAMPLE :- 5
+
+        // public class EvenOdd {
+        //     public static void main(String[] args) {
+        //     int num = 5;
+        //     if(num % 2 == 0)
+        //     System.out.println(num + " is even.");
+        //     else
+        //     System.out.println(num + " is odd.");
+        //     }
+        //    }
+          
+           
+            //  EXAMPLE :- 6
+
+            // public class Factorial{
+            //     public static void main(String[] args) {
+            //     int num = 5, factorial = 1;
+            //     for(int i = 1; i <= num; ++i) {
+            //     factorial *= i;
+            //     }
+            //     System.out.println("Factorial of "+ num + "is: "+ factorial);
+            //     }
+            //    }
+               
+
+            //  EXAMPLE :- 7
+
+            // public class Factorial{
+            //     public static void main(String[] args) {
+            //         int row = 5;
+            //         for (int i = 0; i < row; i++) {
+            //             for (int j = 0; j < i; j++) {
+            //                 System.out.print("*");
+
+                            
+            //             }
+            //             System.out.println();
+                        
+            //         }
+            //     }
+                
+            // }    
+            
+            
+            // //  EXAMPLE :- 8
+
+            // public class AddBinaryNumbers {
+            //     public static void main(String[] args) {
+            //         long binary1 = 1010, binary2 = 1101;
+            //         int i = 0, remainder = 0;
+            //         long[] sum = new long[20];
+            
+            //         while (binary1 != 0 || binary2 != 0) {
+            //             sum[i++] = (binary1 % 10 + binary2 % 10 + remainder) % 2;
+            //             remainder = (int) (binary1 % 10 + binary2 % 10 + remainder) / 2;
+            //             binary1 = binary1 / 10;
+            //             binary2 = binary2 / 10;
+            //         }
+            
+            //         if (remainder != 0) {
+            //             sum[i++] = remainder;
+            //         }
+            
+            //         System.out.print("Sum of two binary numbers: ");
+            //         while (i > 0) {
+            //             System.out.print(sum[--i]);
+            //         }
+            //     }
+            // }
+            
+             
+            // //  EXAMPLE :- 9
+            
